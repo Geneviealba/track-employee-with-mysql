0e7b7d971d048f784265f1b5dbc5c91020cfb370
Genevie Giko <gikogenevie@yahoo.com>
2022-03-02 17:46:04 -0500

    codes update

--- db/schema.sql
+++ db/schema.sql
@@ -43,11 +43,11 @@ VALUE ("Legal");
 
 --Employee role seeds -------
 INSERT INTO role (title, salary, department_id)
-VALUE ("Lead Engineer", 150000, 2);
+VALUE ("Lead Engineer", 170000, 2);
 INSERT INTO role (title, salary, department_id)
 VALUE ("Legal Team Lead", 250000, 4);
 INSERT INTO role (title, salary, department_id)
-VALUE ("Accountant", 125000, 3);
+VALUE ("Accountant", 130000, 3);
 INSERT INTO role (title, salary, department_id)
 VALUE ("Sales Lead", 100000, 1);
 INSERT INTO role (title, salary, department_id)
@@ -59,19 +59,19 @@ VALUE ("Lawyer", 190000, 4);
 
 -- EMPLOYEE SEEDS -------
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Albert", "Haze", null, 1);
+VALUE ("Albert", "Green", null, 1);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tiffany", "Patric", null, 2);
+VALUE ("Faith", "Luv", null, 2);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Mia","Lam",null,3);
+VALUE ("Maya","Hill",null,3);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Bently", "Lao", 1, 4);
+VALUE ("Olivia", "Dream", 1, 4);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Chris", "Melby", 4, 5);
+VALUE ("Tori", "Williams", 4, 5);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Jason", "Baker", 1, 6);
+VALUE ("John", "Brown", 1, 6);
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUE ("Tom", "Nice", 2, 7);
+VALUE ("Tommy", "Gray", 2, 7);
 
 -- SELECTING FOR CREATING 
 --TABLES IN OUR SQL WORKBENCH 
